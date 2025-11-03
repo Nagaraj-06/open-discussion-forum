29c520c5230daf1a4231904fa6751f89c1cfe752
Mass-Nagaraj <rnagaraj6699@gmail.com>
2025-11-03 23:06:52 +0530

    added httpOnly: true

--- backend/src/Routes/auth/routes.js
+++ backend/src/Routes/auth/routes.js
@@ -101,7 +101,7 @@ router.get(
             );
 
             res.cookie("token", token, {
-              httpOnly: false,
+              httpOnly: true,
               secure: true, // true for HTTPS only
               sameSite: "None", // important for cross-site cookies
             });
